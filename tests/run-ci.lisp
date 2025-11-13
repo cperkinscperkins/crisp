bed9b96df08b626d24ade98bc7ebe5cc559aea8e
cperkinscperkins <cperkins.mobile@gmail.com>
2025-11-12 16:59:50 -0800

    namespace error

--- tests/run-ci.lisp
+++ tests/run-ci.lisp
@@ -15,18 +15,18 @@
 
 ;; 3. Test the semantic analyzer and codegen path (Target #5/6)
 (format t "~&; --- Running test for (def-function ... 7) ---~%")
-(crisp.compiler:generate-llvm-ir
+(crisp.compiler::generate-llvm-ir
  (crisp.compiler:def-function test-fn-7 ()
    (declare (return-type int)) 7))
 
 (format t "~&; --- Running test for (def-function ... (+ a b)) ---~%")
-(crisp.compiler:generate-llvm-ir
+(crisp.compiler::generate-llvm-ir
  (crisp.compiler:def-function test-fn-add (a b)
    (declare (type a b int) (return-type int))
    (+ a b)))
 
 (format t "~&; --- Running test for arrow syntax (def-function ... #') ---~%")
-(crisp.compiler:generate-llvm-ir
+(crisp.compiler::generate-llvm-ir
  (crisp.compiler:def-function test-fn-arrow (a b)
    (declare #'(int int => int))
    (+ a b)))
