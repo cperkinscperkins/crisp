3b3986d59de96ae93b9f871fc07f4ab2b3f92e02
cperkinscperkins <cperkins.mobile@gmail.com>
2025-11-16 09:42:54 -0800

    fix tests with new interface

--- tests/run-ci.lisp
+++ tests/run-ci.lisp
@@ -24,23 +24,32 @@
 (format t "~&; --- Running test-llvm-hello-world ---~%")
 (test-llvm-hello-world)
 
+(defun test-compile-and-print (semantic-fn)
+  "Helper to create a module, compile a function, and print the IR."
+  (let ((module (llvm-module-create "ci_test_module"))
+        (builder (llvm-create-builder)))
+    (unwind-protect
+         (progn
+           (generate-llvm-ir semantic-fn module builder)
+           (let ((ir-ptr (llvm-print-module-to-string module)))
+             (unwind-protect
+                  (format t "~a~%" (cffi:foreign-string-to-lisp ir-ptr))
+               (llvm-dispose-message ir-ptr))))
+      (llvm-dispose-builder builder)
+      (llvm-dispose-module module))))
+
 ;; test the semantic analyzer and codegen path (Target #5/6)
 (format t "~&; --- Running test for (def-function ... 7) ---~%")
-(generate-llvm-ir
- (def-function test-fn-7 ()
-   (declare (return-type int)) 7))
+(test-compile-and-print
+ (internal-def-function 'test-fn-7 '() '((return-type int)) '(7) '(0)))
 
 (format t "~&; --- Running test for (def-function ... (+ a b)) ---~%")
-(generate-llvm-ir
- (def-function test-fn-add (a b)
-   (declare (type a b int) (return-type int))
-   (+ a b)))
+(test-compile-and-print
+ (internal-def-function 'test-fn-add '(a b) '((type a b int) (return-type int)) '((+ a b)) '(1)))
 
 (format t "~&; --- Running test for arrow syntax (def-function ... #') ---~%")
-(generate-llvm-ir
- (def-function test-fn-arrow (a b)
-   (declare #'(int int => int))
-   (+ a b)))
+(test-compile-and-print
+ (internal-def-function 'test-fn-arrow '(a b) '((function (int int => int))) '((+ a b)) '(2)))
 
 ;; yay
 (format t "~&~%*** Crisp CI Tests Passed! ***~%")
