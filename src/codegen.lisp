e888a183ab36fbd1fdf6fd967db5f7876c37876b
cperkinscperkins <cperkins.mobile@gmail.com>
2025-11-21 22:35:59 -0800

    copyright and update

--- src/codegen.lisp
+++ src/codegen.lisp
@@ -1,3 +1,9 @@
+;;;; Crisp - Lisp for Developing GPU Kernels
+;;;; Copyright (c) 2025 Christopher Perkins
+;;;;
+;;;; Licensed under the MIT License. See LICENSE file in the project root.
+
+
 ;; src/codegen.lisp
 (in-package :crisp.compiler)
 
