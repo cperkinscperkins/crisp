f55c338b1c37ed3065f0c6c2873b7773ae4f5d80
Chris Perkins <cperkins.mobile@gmail.com>
2025-12-28 14:49:21 -0800

    encapsultate param logic refactor

---
+++ src/parameters.lisp
@@ -0,0 +1,25 @@
+;;;; src/parameters.lisp
+(in-package :crisp.compiler)
+
+;;; =========================================================
+;;; Parameter Encapsulation
+;;; =========================================================
+
+(defstruct parameter-def
+  "Represents a function parameter with its type, kind, and metadata.
+   Replaces the legacy list format: (name type :kind kind ...)"
+  (name nil :type symbol)
+  (type 't :type (or symbol list))
+  (kind :in :type keyword) ;; :in, :out, :in-out
+  (is-optional nil :type boolean)
+  (is-key nil :type boolean)
+  (default-value nil)
+  (source-location nil))
+
+(defmethod print-object ((obj parameter-def) stream)
+  (print-unreadable-object (obj stream :type t :identity nil)
+    (format stream "~a ~a~@[ :kind ~a~]"
+            (parameter-def-name obj)
+            (parameter-def-type obj)
+            (cl:unless (eq (parameter-def-kind obj) :in)
+              (parameter-def-kind obj)))))
