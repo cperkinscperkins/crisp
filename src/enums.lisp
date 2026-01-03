71a1cf992b9577a87e4d218583338fd70747ff38
Chris Perkins <cperkins.mobile@gmail.com>
2026-01-02 20:52:50 -0800

    address space changes

--- src/enums.lisp
+++ src/enums.lisp
@@ -49,12 +49,29 @@
 
 ;;; Standard Enumerations
 
-(def-enumeration address-space
-                 (:unknown 0)
-                 (:global 1)
-                 :local
-                 :private
-                 :constant)
+;;; Standard Enumerations
+;;; Note: We manually define address-space to support target-sensitive value resolution
+;;; while keeping the enumeration registry populated for validation.
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf (gethash 'address-space *crisp-enums*)
+    (make-enumeration-def :name 'address-space
+                          :members '((:private . 0)
+                                     (:global . 1)
+                                     (:constant . 2)
+                                     (:local . 3)
+                                     (:generic . 4)))))
+
+(defun is-address-space? (x)
+  (or (assoc x '((:private . 0) (:global . 1) (:constant . 2) (:local . 3) (:generic . 4)))
+      (and (integerp x)
+           (find x '(0 1 2 3 4)))))
+
+(defun address-space-value (k)
+  "Returns the integer value for an address space keyword, sensitive to *target-backend*."
+  (encode-address-space k))
+
+(export 'is-address-space?)
 
 (def-enumeration access
                  (:unknown 255)
